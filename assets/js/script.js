fcbe9f4df7f062979e917a41b85cb706d844c326
becode <mehdi.semlali.1988@gmail.com>
2020-06-27 15:34:56 +0200

    ajout fonction delete + commentaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
 (() => {
-
-    async function showCaracter() {
-        fetch('https://character-database.becode.xyz/characters')
-            .then((data)=>data.json())
-            .then((res)=>{console.table(res)})
+//Fonction show les carateres
+    async function showCaracters() {
+        fetch('characters')
+            .then((data) => data.json())
+            .then((res) => { console.table(res) })
     }
+// au clique fonction delete
+    document.querySelector("#delete").addEventListener("click", () => {
+        charactersId = document.querySelector("#ID").value;
+        deleteCharacters(charactersId);
+    });
+// au clique fonction show
+    document.querySelector("#run").addEventListener("click", () => {
 
-    document.querySelector(".run").addEventListener("click", () => {
-
-        showCaracter();
+        showCaracters();
 
     })
 
+// -fonction delete
+    const deleteCharacters = id => {
+        const charactersObject = JSON.stringify({
+            id: id
+        });
+
+        let url = "characters" + charactersId;
+        fetch(url, {
+            method: "DELETE",
+            headers: new Headers({
+                "content-type": "application/json"
+            })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+        console.log(charactersId);
+    };
+
 })();
+
 
