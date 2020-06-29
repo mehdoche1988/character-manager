d1a1bd1af9e9a36a74f7d906d4e6e2aa68d2a7af
becode <mehdi.semlali.1988@gmail.com>
2020-06-29 15:13:50 +0200

    test ajout delete OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,29 @@
-document.getElementById("submit").addEventListener("click", async () => {
+let name = document.querySelector('#nameRecup');
+let desc = document.querySelector('#desc');
+let shortDescriptionRecup = document.querySelector('#shortDescriptionRecup');
 
 
-    let name = document.querySelector('#nameRecup');
-    let desc = document.querySelector('#desc');
-    let shortDescriptionRecup = document.querySelector('#shortDescriptionRecup');
-    let imageRecup = document.querySelector('#imageRecup');
-    let Carts = {
+document.getElementById("form").addEventListener("submit", (e) => {
+
+    e.preventDefault()
+ console.log("test")
+  
+ fetch(
+    "https://character-database.becode.xyz/characters",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        image:
+          "R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",
         name: name.value,
         description: desc.value,
         shortDescription: shortDescriptionRecup.value,
-        image: imageRecup.value
+      }),
     }
-    console.log(Carts);
-
-    await fetch("https://character-database.becode.xyz/characters", 
-    {
-        
-        method: "POST",
-        body: JSON.stringify(Carts),
-        headers: new Headers({
-            "Content-Type": "application/json"
-        })
-    });
-
-    console.log("je suis un test");
+  );
 
 });
-
-
