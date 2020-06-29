98536bc51173ebc055e3c4f1e8cc7ca55ba23d16
becode <mehdi.semlali.1988@gmail.com>
2020-06-29 08:59:31 +0200

    ajout divers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,43 @@
 (() => {
-//Fonction show les carateres
-    async function showCaracters() {
-        fetch('characters')
-            .then((data) => data.json())
-            .then((res) => { console.table(res) })
-    }
-// au clique fonction delete
-    document.querySelector("#delete").addEventListener("click", () => {
-        charactersId = document.querySelector("#ID").value;
-        deleteCharacters(charactersId);
-    });
+//Fonction show  tout les carateres
+
+document.querySelector("#run").addEventListener("click", () => {
+
+    fetch('https://character-database.becode.xyz/characters')
+        .then((data)=>data.json())
+        .then((res)=>{console.log(res)})
+});
+
+// // au clique fonction delete
+//     document.querySelector("#delete").addEventListener("click", () => {
+//         charactersId = document.querySelector("#ID").value;
+//         deleteCharacters(charactersId);
+//     });
 // au clique fonction show
-    document.querySelector("#run").addEventListener("click", () => {
+    // document.querySelector("#run").addEventListener("click", () => {
 
-        showCaracters();
+    //     showCaracters();
 
-    })
+    // })
 
-// -fonction delete
-    const deleteCharacters = id => {
-        const charactersObject = JSON.stringify({
-            id: id
-        });
+// // -fonction delete
+//     const deleteCharacters = id => {
+//         const charactersObject = JSON.stringify({
+//             id: id
+//         });
 
-        let url = "characters" + charactersId;
-        fetch(url, {
-            method: "DELETE",
-            headers: new Headers({
-                "content-type": "application/json"
-            })
-        })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(err => console.error(err));
-        console.log(charactersId);
-    };
+//         let url = "characters" + charactersId;
+//         fetch(url, {
+//             method: "DELETE",
+//             headers: new Headers({
+//                 "content-type": "application/json"
+//             })
+//         })
+//             .then(response => response.json())
+//             .then(data => console.log(data))
+//             .catch(err => console.error(err));
+//         console.log(charactersId);
+//     };
 
 })();
 
