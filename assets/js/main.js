b83abf877aa3537181ef0f47d5b6dfbbd821a08d
becode <mehdi.semlali.1988@gmail.com>
2020-07-01 09:14:57 +0200

    copie/coller pcq le pull ne fonctionnais pas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
 let boxs = document.getElementById("");
 let modal = document.querySelector(".content-show");
 let deletBtns = document.querySelectorAll("#delete");
-
 let nameEdit = document.querySelector("#name-edit");
 let descEdit = document.querySelector("#desc-edit");
 let shortDescEdit = document.querySelector("#shortDescription-edit");
 
-
-let fetchData = async () => await (await fetch("https://character-database.becode.xyz/characters")).json();
-let fetchDataById = async (id) => await (await fetch("https://character-database.becode.xyz/characters/" + id)).json();
+let fetchData = async () =>
+  await (
+    await fetch("https://character-database.becode.xyz/characters")
+  ).json();
+let fetchDataById = async (id) =>
+  await (
+    await fetch("https://character-database.becode.xyz/characters/" + id)
+  ).json();
 
 async function getCharacters() {
   let outPut = "";
@@ -33,22 +37,15 @@ async function getCharacters() {
           </div>`;
     });
     document.querySelector(".content").innerHTML = outPut;
-    deleteElement()
-
-
+    getInfoCharcters();
+    deleteElement();
+    editCharcters();
   });
-
 }
-getCharacters();
 
-
-
-async function getInfoCharcters() {
-
-  await getCharacters();
+function getInfoCharcters() {
   let boxs = document.querySelectorAll("#show");
   boxs.forEach((box) => {
-
     box.addEventListener("click", async (e) => {
       let id = e.target.parentElement.parentElement.id;
       let character = await fetchDataById(id);
@@ -72,12 +69,9 @@ async function getInfoCharcters() {
       });
     });
   });
-
-
 }
-getInfoCharcters();
 
-async function deleteElement() {
+function deleteElement() {
   let btnDeletes = document.querySelectorAll("#delete");
   btnDeletes.forEach((btn) => {
     btn.addEventListener("click", async (e) => {
@@ -86,9 +80,14 @@ async function deleteElement() {
         .querySelector("#btnDelModal")
         .addEventListener("click", async () => {
           document.querySelector(".modalDelete").classList.remove("open");
-          await fetch("https://character-database.becode.xyz/characters/" + e.target.parentElement.parentElement.id,
+          await fetch(
+            "https://character-database.becode.xyz/characters/" +
+              e.target.parentElement.parentElement.id,
             {
               method: "DELETE",
+              headers: {
+                "Content-type": "application/json; charset=UTF-8",
+              },
             }
           );
           getCharacters();
@@ -100,82 +99,47 @@ async function deleteElement() {
   });
 }
 
-
-
-
-async function editCharcters() {
-  await getCharacters();
-  await getInfoCharcters();
+function editCharcters() {
   let idChar;
-  let resutEdit = document.querySelectorAll('#edit');
-  resutEdit.forEach(ele => {
-    ele.addEventListener('click', async (e) => {
-      document.querySelector(".content-edit").classList.add("open")
+  let resutEdit = document.querySelectorAll("#edit");
+  resutEdit.forEach((ele) => {
+    ele.addEventListener("click", async (e) => {
+      document.querySelector(".content-edit").classList.add("open");
       idChar = e.target.parentElement.parentElement.id;
-      let charachter = await fetchDataById(idChar)
+      let charachter = await fetchDataById(idChar);
       nameEdit.value = charachter.name;
       descEdit.value = charachter.description;
       shortDescEdit.value = charachter.shortDescription;
-      updateCharById(idChar)
-      console.log(idChar)
-
-    })
-
-
-  })
+      editCharctersById(idChar);
+    });
+  });
 }
 
-
-
-
-editCharcters();
-
-
-
-
-
-// PUT to the resource with id = 5 to change the name of task
-/*fetch("https://jsonplaceholder.typicode.com/todos/5",
-method: "PUT",
-body: JSON.stringify({
-userId: 1,
-id: 5,
-title: "test",
-completed: false
-})
-
-
-)*/
-
-async function updateCharById(idCahr) {
+async function editCharctersById(idCahr) {
   let imgurl;
   document.getElementById("form-edit").addEventListener("submit", async (e) => {
-    e.preventDefault()
-    document.querySelector(".content-edit").classList.remove("open")
+    e.preventDefault();
+    document.querySelector(".content-edit").classList.remove("open");
     await fetchDataById(idCahr).then((data) => {
-      imgurl = data.image
-    })
-
-
+      imgurl = data.image;
+    });
     await fetch("https://character-database.becode.xyz/characters/" + idCahr, {
       method: "PUT",
       image: imgurl,
       body: JSON.stringify({
         name: nameEdit.value,
         description: descEdit.value,
-        shortDescription: shortDescEdit.value
-
+        shortDescription: shortDescEdit.value,
       }),
-
       headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
-  })
-
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(() => {
+      getCharacters();
+    });
+  });
 }
-
 document.querySelector("#close-btn-edit").addEventListener("click", () => {
-
-  document.querySelector(".content-edit").classList.remove("open")
-})
+  document.querySelector(".content-edit").classList.remove("open");
+});
+getCharacters();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,23 @@
-let name = document.querySelector('#nameRecup');
-let desc = document.querySelector('#desc');
-let shortDescriptionRecup = document.querySelector('#shortDescriptionRecup');
+let name = document.querySelector("#nameRecup");
+let desc = document.querySelector("#desc");
+let shortDescriptionRecup = document.querySelector("#shortDescriptionRecup");
+let form = document.getElementById("form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-document.getElementById("form").addEventListener("submit", (e) => {
-
-    e.preventDefault()
-
-    console.log("test")
-
-    fetch(
-        "https://character-database.becode.xyz/characters",
-        {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                image: "R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",
-                name: name.value,
-                description: desc.value,
-                shortDescription: shortDescriptionRecup.value,
-            }),
-        }
-    );
-
+  fetch("https://character-database.becode.xyz/characters", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      image:
+        "R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",
+      name: name.value,
+      description: desc.value,
+      shortDescription: shortDescriptionRecup.value,
+    }),
+  });
 });
-
-
-document.getElementById("form-edit").addEventListener("submit", (e) => {
-
-
-
-    console.log("form submi for edit")
-})
-
-
-
-
-
